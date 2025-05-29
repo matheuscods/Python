fb936de71613ebd416de8132dc20e4ce058ce5f5
matheuscods <matheus.santos.maggio@gmail.com>
2025-05-29 01:37:23 -0300

    Desafio 001

---
+++ desafios_python/desafio_001/README.md
@@ -0,0 +1,4 @@
+"""
+Desafio 003 proposto pelo Professor Guanabara (aula 06)
+-> Crie um programa que leia dois números e mostre a soma entre eles; 
+"""
---
+++ desafios_python/desafio_001/desafio_001.py
@@ -0,0 +1,8 @@
+"""
+Desafio 003 proposto pelo Professor Guanabara (aula 06)
+-> Crie um programa que leia dois números e mostre a soma entre eles; 
+"""
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("DIgite o Segundo número: "))
+soma = n1 + n2
+print("A soma entre {} e {} é igual a: {}".format(n1,n2,soma))
